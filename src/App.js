b93ec85213720bfe4c31b64321b07f88703c21c0
Anastasia Drobitko <drobitko91@gmail.com>
2023-04-10 01:27:56 +0300

    added new folders and some components for header block

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 // import logo from './logo.svg';
+// ----------------------------------------------------
+
 
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// import routes
+import Home from "./routes/Home";
+// import About from "./routes/About";
+import HeroDetails from "./routes/HeroDetails";
+
+// import compopnets
+import Navbar from "./compopents/Navbar";
 
 function App() {
   return (
    <Router>
+    <Navbar/>
     <Routes>
-      <Route to={'/'} element={<Home/>}/>
-      <Route to={'about'} element={<About/>}/>
+      <Route path={'/'} element={<Home/>}/>
+      {/* <Route path={'/about'} element={<About/>}/> */}
+      <Route path={'/:id'} element={<HeroDetails/>}/>
     </Routes>    
    </Router>
   );
---
+++ src/compopents/SearchBar.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react'
+
+export default function SearchBar({setter}) {
+    let input = useRef('');
+
+  const handleClick =async (e) => {
+    let value = input.current.value
+    if (value === '') return
+
+    try {
+        let heroes = await fetchHeroes(value)
+        setter(heroes)
+    } catch(err) {
+        return console.error(err);
+    }
+  }
+  return (
+    <form>
+        <input type="text" placeholder='Search Hero...' ref={input} />
+        <button onClick={handleClick}>Search</button>
+    </form>
+  )
+}
