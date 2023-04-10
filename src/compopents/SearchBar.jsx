85e872ab5779486d64d5ec3278b9e325dc60cf20
Anastasia Drobitko <drobitko91@gmail.com>
2023-04-11 00:59:20 +0300

    added charachters and some data

--- src/compopents/SearchBar.jsx
+++ src/compopents/SearchBar.jsx
@@ -1,19 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
+
+// import utils
+import { fetchHeroes } from '../utils/utils';
 
 export default function SearchBar({setter}) {
     let input = useRef('');
 
-  const handleClick =async (e) => {
-    let value = input.current.value
-    if (value === '') return
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let value = input.current.value;
+    if (value === '') return;
 
     try {
-        let heroes = await fetchHeroes(value)
-        setter(heroes)
+        let heroes = await fetchHeroes(value);
+        setter(heroes);
     } catch(err) {
         return console.error(err);
     }
-  }
+  };
   return (
     <form>
         <input type="text" placeholder='Search Hero...' ref={input} />
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,24 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 
 // import components
 import Container from '../compopents/Container';
 import SearchBar from '../compopents/SearchBar';
 import Grid from '../compopents/Grid';
+import Card from '../compopents/Card';
+
+const IMAGE_SIZE = 'portrait_fantastic';
 
 export default function Home() {
-    const [heroes, setHeroes] = useState([])
-  return (
-    <Container>
-      <h1>Discover Marvel Heroes</h1>
-      <SearchBar setter={setHeroes} />
-      <Grid>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-      </Grid>
-    </Container>
-  );
+    const [heroes, setHeroes] = useState([]);
+
+    let cards;
+
+    if (heroes) {
+      cards = heroes.map((hero) => {
+        <Card name={hero.name} id={hero.id} key={hero.id} thumbnail={`${hero.thumbnail.path}/${IMAGE_SIZE}.${hero.thumbnail.extension}`}/>
+      } );
+    }
+
+    return (
+      <Container>
+        <h1>Discover Marvel Heroes</h1>
+        <SearchBar setter={setHeroes}/>
+        <Grid>
+          {cards ? cards : ''}
+        </Grid>
+      </Container>
+    );
 }
